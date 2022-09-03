1dc6cf332d4b9312db51cdeca607b719d54d521e
Gurix525 <gurix525@gmail.com>
2022-09-03 19:14:49 +0200

    Cleaned a little and created NavMesh

---
+++ MicroSpace/Assets/Code/Pathfinding/NavMesh.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Code.Pathfinding
+{
+    public class NavMesh : MonoBehaviour
+    {
+        [SerializeField] private Agent[] _agents;
+    }
+}
