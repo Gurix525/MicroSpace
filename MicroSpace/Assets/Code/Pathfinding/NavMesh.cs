e7bd2d95b00be30dfb7fd33ee19d31a40b2ffadf
Gurix525 <gurix525@gmail.com>
2022-09-07 19:15:02 +0200

    Triangulacja zrobiona tak na 4.5/5

--- MicroSpace/Assets/Code/Pathfinding/NavMesh.cs
+++ MicroSpace/Assets/Code/Pathfinding/NavMesh.cs
@@ -3,6 +3,7 @@ using DelaunatorSharp;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 namespace Assets.Code.Pathfinding
 {
@@ -11,10 +12,14 @@ namespace Assets.Code.Pathfinding
         [SerializeField] private List<Agent> _agents;
         [SerializeField] private float _navMeshSizeFromCenter = 50;
         [SerializeField] private Material _simpleMaterial;
-        [SerializeField] private float _navBorder = 0.5f;
+        [SerializeField] private float _navBorder = 0.5F;
+        [SerializeField] private float _verticeSize = 0.1F;
+        [SerializeField] private float _lineDrawTime = 5F;
 
         private List<Vector2> _vertices = new();
         private BoxCollider2D[] _colliders = { };
+
+        private float _vOffset => _verticeSize / 2;
 
         private void FixedUpdate()
         {
@@ -41,10 +46,50 @@ namespace Assets.Code.Pathfinding
                 Points[i] = _vertices[i].ToPoint();
             Delaunator delaunator = new(Points);
 
-            delaunator.ForEachTriangle(x => DrawTriangle((Triangle)x));
+            // Rysowanie do debugowania
+            int lineCount = 0;
+
+            delaunator.ForEachTriangle(x => DrawTriangle((Triangle)x, delaunator));
+            //_vertices.ForEach(x => DrawVertice(x));
+            delaunator.ForEachTriangleEdge(x => DrawEdge(x, ref lineCount));
         }
 
-        private void DrawTriangle(Triangle triangleToDraw)
+        private void DrawEdge(IEdge edge, ref int lineCount)
+        {
+            lineCount++;
+            Debug.Log(lineCount);
+            Debug.DrawLine(((Point)edge.P).ToVector2(), ((Point)edge.Q).ToVector2(),
+                Color.red, _lineDrawTime);
+        }
+
+        private void DrawVertice(Vector2 v)
+        {
+            GameObject vertice = new GameObject();
+            vertice.transform.parent = transform;
+            var mesh = vertice.AddComponent<MeshFilter>().mesh;
+            var renderer = vertice.AddComponent<MeshRenderer>();
+            mesh.Clear();
+            mesh.vertices = new Vector3[]
+            {
+                (Vector3)v + new Vector3(_vOffset, _vOffset, -1),
+                (Vector3)v + new Vector3(-_vOffset, _vOffset, -1),
+                (Vector3)v + new Vector3(_vOffset, -_vOffset, -1),
+                (Vector3)v + new Vector3(-_vOffset, -_vOffset, -1)
+            };
+            mesh.uv = new Vector2[]
+            {
+                v + new Vector2(_vOffset, _vOffset),
+                v + new Vector2(-_vOffset, _vOffset),
+                v + new Vector2(_vOffset, -_vOffset),
+                v + new Vector2(-_vOffset, -_vOffset)
+            };
+            mesh.triangles = new int[] { 0, 2, 1, 2, 3, 1 };
+
+            renderer.material = new Material(_simpleMaterial);
+            renderer.material.color = Color.red;
+        }
+
+        private void DrawTriangle(Triangle triangleToDraw, Delaunator delaunator)
         {
             GameObject triangle = new GameObject();
             triangle.transform.parent = transform;
@@ -73,31 +118,47 @@ namespace Assets.Code.Pathfinding
 
             renderer.material = new Material(_simpleMaterial);
 
-            bool isForbidden = false;
+            renderer.material.color = Color.HSVToRGB(
+                    (float)rand.Next(3600) / 3600,
+                    (float)rand.Next(20, 55) / 100,
+                    (float)rand.Next(85, 95)) / 100;
 
-            foreach (var collider in _colliders)
+            var centroid = delaunator.GetCentroid(triangleToDraw.Index);
+
+            foreach (var box in _colliders)
             {
-                int i = 0;
-                foreach (var point in mesh.vertices)
+                if (Vector2.Distance(((Point)centroid).ToVector2(), box.transform.position) < 2 * _navBorder)
                 {
-                    if (Vector2.Distance(point, collider.transform.position) > 1.273F)
-                        break;
-                    i++;
-                }
-                if (i == 3)
-                {
-                    isForbidden = true;
-                    break;
+                    renderer.material.color = Color.black;
+                    return;
                 }
             }
 
-            if (!isForbidden)
-                renderer.material.color = Color.HSVToRGB(
-                    (float)rand.Next(3600) / 3600,
-                    (float)rand.Next(20, 35) / 100,
-                    (float)rand.Next(85, 95)) / 100;
-            else
-                renderer.material.color = Color.black;
+            //bool isForbidden = false;
+
+            //foreach (var collider in _colliders)
+            //{
+            //    int i = 0;
+            //    foreach (var point in mesh.vertices)
+            //    {
+            //        if (Vector2.Distance(point, collider.transform.position) > 1.273F)
+            //            break;
+            //        i++;
+            //    }
+            //    if (i == 3)
+            //    {
+            //        isForbidden = true;
+            //        break;
+            //    }
+            //}
+
+            //if (!isForbidden)
+            //    renderer.material.color = Color.HSVToRGB(
+            //        (float)rand.Next(3600) / 3600,
+            //        (float)rand.Next(20, 35) / 100,
+            //        (float)rand.Next(85, 95)) / 100;
+            //else
+            //    renderer.material.color = Color.black;
         }
 
         private void FindVertices()
@@ -133,6 +194,12 @@ namespace Assets.Code.Pathfinding
             foreach (var item in _vertices)
             {
                 if (Vector2.Distance(item, v) < 0.01F) return;
+            }
+            foreach (var item in _colliders)
+            {
+                Vector2 cp = item.transform.position;
+                if (Vector2.Distance(cp, v) < 0.45F + _navBorder)
+                    return;
             }
             _vertices.Add(v);
         }
