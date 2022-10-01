7c53ebe6aa229cdfabdef3d1f951155cee303195
Gurix525 <gurix525@gmail.com>
2022-10-01 23:56:13 +0200

    Refaktoryzacja i dokumentacja pathfindingu #1

---
+++ MicroSpace/Assets/Code/Pathfinding/Line.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Code.Pathfinding
+{
+    public struct Line
+    {
+        public static readonly Line Zero = new Line(Vector2.zero, Vector2.zero);
+
+        public Vector2 A { get; }
+        public Vector2 B { get; }
+
+        public Line(Vector2 a, Vector2 b)
+        {
+            A = a;
+            B = b;
+        }
+    }
+}
--- MicroSpace/Assets/Code/Pathfinding/NavMesh.cs
+++ MicroSpace/Assets/Code/Pathfinding/NavMesh.cs
@@ -10,20 +10,38 @@ namespace Assets.Code.Pathfinding
 {
     public class NavMesh : MonoBehaviour
     {
-        [SerializeField] private List<Agent> _agents;
-        private BoxCollider2D[] _colliders = { };
-        private List<(Vector2 A, Vector2 B)> _invalidEdges = new();
-        [SerializeField] private float _lineDrawTime = 5F;
-        [SerializeField] private float _navBorder = 0.5F;
-        [SerializeField] private float _navMeshSizeFromCenter = 50;
-        private List<Node> _nodes = new();
-        private List<Vector2> _nodesPositions = new();
-        [SerializeField] private Material _simpleMaterial;
-        private List<(Vector2 A, Vector2 B)> _validEdges = new();
-        private List<Vector2> _vertices = new();
-        [SerializeField] private float _verticeSize = 0.1F;
-        private float _vOffset => _verticeSize / 2;
+        #region Fields
 
+        private const float _navBorder = 0.45F; // 0.45F
+        private const float _navMeshSizeFromCenter = 50F; // 50F
+
+        [Header("DEBUG ONLY")]
+        [SerializeField]
+        private Material _simpleMaterial;
+
+        [SerializeField]
+        private float _lineDrawTime = 2F;
+
+        [SerializeField]
+        private float _verticeSize = 0.1F;
+
+        private List<Node> _nodes = new();
+        private BoxCollider2D[] _colliders = { };
+        private List<Vector2> _nodesPositions = new();
+        private List<Vector2> _vertices = new();
+        private List<(Vector2 A, Vector2 B)> _invalidEdges = new();
+
+        #endregion Fields
+
+        #region Properties
+
+        private float _vOffset => _verticeSize / 2; // Debug only
+
+        #endregion Properties
+
+        #region Public
+
+        // Skrócić
         public Path FindPath(Vector2 startPos, Vector2 endPos)
         {
             Node start = FindClosestNode(startPos);
@@ -36,7 +54,7 @@ namespace Assets.Code.Pathfinding
 
             open.Add(new(start, 0, 0, null));
 
-            IterateOverNodes(open, closed, endPos, end);
+            CalculateNodes(open, closed, endPos, end); // A*
 
             var finalNode = closed.Find(x => x.Node == end);
 
@@ -61,6 +79,11 @@ namespace Assets.Code.Pathfinding
             else return null;
         }
 
+        #endregion Public
+
+        #region Private
+
+        // Wyjaśnić
         private void AddBoxVertice(BoxCollider2D boxCollider, Vector3 offset)
         {
             Vector2 v = (boxCollider.transform.position + offset).
@@ -78,6 +101,7 @@ namespace Assets.Code.Pathfinding
             _vertices.Add(v);
         }
 
+        // Skrócić i wyjaśnić
         private void AddEdgesFromTriangle(Triangle triangle,
             List<(Vector2, Vector2)> validEdges,
             List<(Vector2, Vector2)> invalidEdges,
@@ -114,11 +138,17 @@ namespace Assets.Code.Pathfinding
             }
         }
 
-        private float CrossProduct2D(Vector2 a, Vector2 b)
+        /// <summary>
+        /// Funkcja dla <code>AreLinesIntersecting()</code>
+        /// </summary>
+        private float GetCrossProduct2D(Vector2 a, Vector2 b)
         {
             return a.x * b.y - b.x * a.y;
         }
 
+        /// <summary>
+        /// Funkcja dla <code>AreLinesIntersecting()</code>
+        /// </summary>
         private void Swap<T>(ref T lhs, ref T rhs)
         {
             T temp = lhs;
@@ -126,12 +156,20 @@ namespace Assets.Code.Pathfinding
             rhs = temp;
         }
 
+        /// <summary>
+        /// Funkcja dla <code>AreLinesIntersecting()</code>
+        /// </summary>
         private bool Approximately(float a, float b, float tolerance = 1e-5F)
         {
             return Mathf.Abs(a - b) <= tolerance;
         }
 
-        private bool AreLinesIntersecting(Vector2 A, Vector2 B, Vector2 C, Vector2 D, out Vector2 intersection)
+        /// <summary>
+        /// Sprawdzenie, czy podane linie AB i CD się przecinają
+        /// </summary>
+        /// <returns>True, jeśli linie się przecinają oraz miejsce przecięcia</returns>
+        private bool AreLinesIntersecting(Vector2 A, Vector2 B,
+            Vector2 C, Vector2 D, out Vector2 intersection)
         {
             if (A == C || A == D || B == C || B == D)
             {
@@ -145,12 +183,12 @@ namespace Assets.Code.Pathfinding
             var s = D - C;
             var qminusp = q - p;
 
-            float cross_rs = CrossProduct2D(r, s);
+            float cross_rs = GetCrossProduct2D(r, s);
 
             if (Approximately(cross_rs, 0f))
             {
                 // Parallel lines
-                if (Approximately(CrossProduct2D(qminusp, r), 0f))
+                if (Approximately(GetCrossProduct2D(qminusp, r), 0f))
                 {
                     // Co-linear lines, could overlap
                     float rdotr = Vector2.Dot(r, r);
@@ -189,8 +227,8 @@ namespace Assets.Code.Pathfinding
             else
             {
                 // Not parallel, calculate t and u
-                float t = CrossProduct2D(qminusp, s) / cross_rs;
-                float u = CrossProduct2D(qminusp, r) / cross_rs;
+                float t = GetCrossProduct2D(qminusp, s) / cross_rs;
+                float u = GetCrossProduct2D(qminusp, r) / cross_rs;
                 if (t >= 0 && t <= 1 && u >= 0 && u <= 1)
                 {
                     intersection = p + t * r;
@@ -205,7 +243,11 @@ namespace Assets.Code.Pathfinding
             }
         }
 
-        private void Awake()
+        /// <summary>
+        /// Dodaje 4 wierzchołki do navmesha na starcie. Potrzebne, żeby navmesh
+        /// zawsze działał
+        /// </summary>
+        private void AddStartingVertices()
         {
             _vertices = new List<Vector2>()
             {
@@ -216,120 +258,18 @@ namespace Assets.Code.Pathfinding
             };
         }
 
-        // DEBUG
-        private void DrawEdge(IEdge edge, Color color)
-        {
-            Debug.DrawLine(((Point)edge.P).ToVector2(), ((Point)edge.Q).ToVector2(),
-                color, _lineDrawTime);
-        }
-
-        // DEBUG
-        private void DrawTriangle(Triangle triangleToDraw, Delaunator delaunator)
-        {
-            GameObject triangle = new GameObject();
-            triangle.transform.parent = transform;
-            var mesh = triangle.AddComponent<MeshFilter>().mesh;
-            var renderer = triangle.AddComponent<MeshRenderer>();
-            mesh.Clear();
-            IPoint[] iPoints = new List<IPoint>(triangleToDraw.Points).ToArray();
-            Point[] points = new Point[iPoints.Length];
-            for (int i = 0; i < iPoints.Length; i++)
-                points[i] = (Point)iPoints[i];
-            mesh.vertices = new Vector3[]
-            {
-                points[0].ToVector2(),
-                points[1].ToVector2(),
-                points[2].ToVector2()
-            };
-            mesh.uv = new Vector2[]
-            {
-                points[0].ToVector2(),
-                points[1].ToVector2(),
-                points[2].ToVector2()
-            };
-            mesh.triangles = new int[] { 0, 1, 2 };
-
-            System.Random rand = new();
-
-            renderer.material = new Material(_simpleMaterial);
-            renderer.material.color = new Color32(0, 0, 127, 0);
-
-            // Random color to triangles
-            //renderer.material.color = Color.HSVToRGB(
-            //        (float)rand.Next(3600) / 3600,
-            //        (float)rand.Next(20, 55) / 100,
-            //        (float)rand.Next(85, 95)) / 100;
-
-            var centroid = delaunator.GetCentroid(triangleToDraw.Index);
-
-            foreach (var box in _colliders)
-            {
-                if (Vector2.Distance(((Point)centroid).ToVector2(), box.transform.position) < 0.05F + 2 * _navBorder)
-                {
-                    renderer.material.color = Color.black;
-                    return;
-                }
-            }
-
-            //bool isForbidden = false;
-
-            //foreach (var collider in _colliders)
-            //{
-            //    int i = 0;
-            //    foreach (var point in mesh.vertices)
-            //    {
-            //        if (Vector2.Distance(point, collider.transform.position) > 1.273F)
-            //            break;
-            //        i++;
-            //    }
-            //    if (i == 3)
-            //    {
-            //        isForbidden = true;
-            //        break;
-            //    }
-            //}
-
-            //if (!isForbidden)
-            //    renderer.material.color = Color.HSVToRGB(
-            //        (float)rand.Next(3600) / 3600,
-            //        (float)rand.Next(20, 35) / 100,
-            //        (float)rand.Next(85, 95)) / 100;
-            //else
-            //    renderer.material.color = Color.black;
-        }
-
-        // DEBUG
-        private void DrawVertice(Vector2 v)
-        {
-            GameObject vertice = new GameObject();
-            vertice.transform.parent = transform;
-            var mesh = vertice.AddComponent<MeshFilter>().mesh;
-            var renderer = vertice.AddComponent<MeshRenderer>();
-            mesh.Clear();
-            mesh.vertices = new Vector3[]
-            {
-                (Vector3)v + new Vector3(_vOffset, _vOffset, -1),
-                (Vector3)v + new Vector3(-_vOffset, _vOffset, -1),
-                (Vector3)v + new Vector3(_vOffset, -_vOffset, -1),
-                (Vector3)v + new Vector3(-_vOffset, -_vOffset, -1)
-            };
-            mesh.uv = new Vector2[]
-            {
-                v + new Vector2(_vOffset, _vOffset),
-                v + new Vector2(-_vOffset, _vOffset),
-                v + new Vector2(_vOffset, -_vOffset),
-                v + new Vector2(-_vOffset, -_vOffset)
-            };
-            mesh.triangles = new int[] { 0, 2, 1, 2, 3, 1 };
-
-            renderer.material = new Material(_simpleMaterial);
-            renderer.material.color = Color.red;
-        }
-
+        /// <summary>
+        ///
+        /// </summary>
+        /// <returns>Najbliższy Node dla podanej pozycji</returns>
         private Node FindClosestNode(Vector2 pos) =>
             _nodes.OrderBy(x => Vector2.Distance(x.Position, pos))
             .ToArray()[0];
 
+        /// <summary>
+        /// Sprawdza, czy trójkąt nie jest za blisko, a jeśli nie jest,
+        /// to dodaje pozycje jego wierzchołków do listy potencjalnych nodów
+        /// </summary>
         private void FindNodes(Triangle triangle, Delaunator delaunator)
         {
             var centroid = delaunator.GetCentroid(triangle.Index);
@@ -349,6 +289,9 @@ namespace Assets.Code.Pathfinding
             }
         }
 
+        /// <summary>
+        /// Wyszukuje wierzchołki colliderów do triangulacji
+        /// </summary>
         private void FindVertices()
         {
             _vertices = new List<Vector2>()
@@ -374,15 +317,10 @@ namespace Assets.Code.Pathfinding
             }
         }
 
-        private void FixedUpdate()
-        {
-            if (_agents.Count > 0)
-                UpdateMesh();
-            transform.eulerAngles = Vector2.zero;
-            transform.parent.GetChild(1).eulerAngles = Vector2.zero;
-        }
-
-        private void IterateOverNodes(List<TempNode> open, List<TempNode> closed,
+        /// <summary>
+        /// Oblicza wartości nodów dla algorytmu A* i nadaje im rodziców
+        /// </summary>
+        private void CalculateNodes(List<TempNode> open, List<TempNode> closed,
             Vector2 endPos, Node end)
         {
             while (open.Count > 0)
@@ -495,30 +433,6 @@ namespace Assets.Code.Pathfinding
                 TraceParent(node.Parent, path);
         }
 
-        private void Update()
-        {
-            //if (Input.GetKeyDown(KeyCode.T))
-            //{
-            //    _colliders = FindObjectsOfType<BoxCollider2D>();
-            //    UpdateMesh();
-            //}
-            //if (Input.GetKeyDown(KeyCode.F))
-            //{
-            //    var path = FindPath(new(-20, -20), new(20, 20));
-            //    //foreach (var item in path.Nodes)
-            //    //    DrawVertice(item.Position);
-            //    if (path != null)
-            //    {
-            //        for (int i = 0; i < path.Nodes.Count - 1; i++)
-            //        {
-            //            DrawEdge(new Edge(0, path.Nodes[i].Position.ToPoint(),
-            //                path.Nodes[i + 1].Position.ToPoint()), Color.green);
-            //        }
-            //    }
-            //    else Debug.Log("Path not found.");
-            //}
-        }
-
         public void UpdateMesh()
         {
             _colliders = FindObjectsOfType<BoxCollider2D>();
@@ -527,7 +441,6 @@ namespace Assets.Code.Pathfinding
             foreach (Transform item in transform)
                 Destroy(item.gameObject);
 
-            float size = _navMeshSizeFromCenter;
             IPoint[] Points = new IPoint[_vertices.Count];
             for (int i = 0; i < Points.Length; i++)
                 Points[i] = _vertices[i].ToPoint();
@@ -544,7 +457,7 @@ namespace Assets.Code.Pathfinding
                 _nodes.Add(new(item));
             }
 
-            _validEdges = new(); // Do wyszukiwania ścieżki
+            List<(Vector2, Vector2)> _validEdges = new(); // Do wyszukiwania ścieżki
             _invalidEdges = new(); // Do skracania ścieżki
             delaunator.ForEachTriangle(x => AddEdgesFromTriangle(
                 (Triangle)x, _validEdges, _invalidEdges, delaunator));
@@ -572,6 +485,49 @@ namespace Assets.Code.Pathfinding
             //    x => DrawEdge(new Edge(0, x.A.ToPoint(), x.B.ToPoint()), Color.red));
         }
 
+        #endregion Private
+
+        #region Unity
+
+        private void Awake()
+        {
+            AddStartingVertices();
+        }
+
+        private void Update()
+        {
+            //if (Input.GetKeyDown(KeyCode.T))
+            //{
+            //    _colliders = FindObjectsOfType<BoxCollider2D>();
+            //    UpdateMesh();
+            //}
+            //if (Input.GetKeyDown(KeyCode.F))
+            //{
+            //    var path = FindPath(new(-20, -20), new(20, 20));
+            //    //foreach (var item in path.Nodes)
+            //    //    DrawVertice(item.Position);
+            //    if (path != null)
+            //    {
+            //        for (int i = 0; i < path.Nodes.Count - 1; i++)
+            //        {
+            //            DrawEdge(new Edge(0, path.Nodes[i].Position.ToPoint(),
+            //                path.Nodes[i + 1].Position.ToPoint()), Color.green);
+            //        }
+            //    }
+            //    else Debug.Log("Path not found.");
+            //}
+        }
+
+        private void FixedUpdate()
+        {
+            transform.eulerAngles = Vector2.zero;
+            transform.parent.GetChild(1).eulerAngles = Vector2.zero;
+        }
+
+        #endregion Unity
+
+        #region Classes
+
         private class TempNode
         {
             public TempNode(Node node, float g, float h, TempNode parent)
@@ -594,5 +550,121 @@ namespace Assets.Code.Pathfinding
                 return Node.ToString();
             }
         }
+
+        #endregion Classes
+
+        #region Debug
+
+        // DEBUG
+        private void DrawEdge(IEdge edge, Color color)
+        {
+            Debug.DrawLine(((Point)edge.P).ToVector2(), ((Point)edge.Q).ToVector2(),
+                color, _lineDrawTime);
+        }
+
+        // DEBUG
+        private void DrawTriangle(Triangle triangleToDraw, Delaunator delaunator)
+        {
+            GameObject triangle = new GameObject();
+            triangle.transform.parent = transform;
+            var mesh = triangle.AddComponent<MeshFilter>().mesh;
+            var renderer = triangle.AddComponent<MeshRenderer>();
+            mesh.Clear();
+            IPoint[] iPoints = new List<IPoint>(triangleToDraw.Points).ToArray();
+            Point[] points = new Point[iPoints.Length];
+            for (int i = 0; i < iPoints.Length; i++)
+                points[i] = (Point)iPoints[i];
+            mesh.vertices = new Vector3[]
+            {
+                points[0].ToVector2(),
+                points[1].ToVector2(),
+                points[2].ToVector2()
+            };
+            mesh.uv = new Vector2[]
+            {
+                points[0].ToVector2(),
+                points[1].ToVector2(),
+                points[2].ToVector2()
+            };
+            mesh.triangles = new int[] { 0, 1, 2 };
+
+            System.Random rand = new();
+
+            renderer.material = new Material(_simpleMaterial);
+            renderer.material.color = new Color32(0, 0, 127, 0);
+
+            // Random color to triangles
+            //renderer.material.color = Color.HSVToRGB(
+            //        (float)rand.Next(3600) / 3600,
+            //        (float)rand.Next(20, 55) / 100,
+            //        (float)rand.Next(85, 95)) / 100;
+
+            var centroid = delaunator.GetCentroid(triangleToDraw.Index);
+
+            foreach (var box in _colliders)
+            {
+                if (Vector2.Distance(((Point)centroid).ToVector2(), box.transform.position) < 0.05F + 2 * _navBorder)
+                {
+                    renderer.material.color = Color.black;
+                    return;
+                }
+            }
+
+            //bool isForbidden = false;
+
+            //foreach (var collider in _colliders)
+            //{
+            //    int i = 0;
+            //    foreach (var point in mesh.vertices)
+            //    {
+            //        if (Vector2.Distance(point, collider.transform.position) > 1.273F)
+            //            break;
+            //        i++;
+            //    }
+            //    if (i == 3)
+            //    {
+            //        isForbidden = true;
+            //        break;
+            //    }
+            //}
+
+            //if (!isForbidden)
+            //    renderer.material.color = Color.HSVToRGB(
+            //        (float)rand.Next(3600) / 3600,
+            //        (float)rand.Next(20, 35) / 100,
+            //        (float)rand.Next(85, 95)) / 100;
+            //else
+            //    renderer.material.color = Color.black;
+        }
+
+        // DEBUG
+        private void DrawVertice(Vector2 v)
+        {
+            GameObject vertice = new GameObject();
+            vertice.transform.parent = transform;
+            var mesh = vertice.AddComponent<MeshFilter>().mesh;
+            var renderer = vertice.AddComponent<MeshRenderer>();
+            mesh.Clear();
+            mesh.vertices = new Vector3[]
+            {
+                (Vector3)v + new Vector3(_vOffset, _vOffset, -1),
+                (Vector3)v + new Vector3(-_vOffset, _vOffset, -1),
+                (Vector3)v + new Vector3(_vOffset, -_vOffset, -1),
+                (Vector3)v + new Vector3(-_vOffset, -_vOffset, -1)
+            };
+            mesh.uv = new Vector2[]
+            {
+                v + new Vector2(_vOffset, _vOffset),
+                v + new Vector2(-_vOffset, _vOffset),
+                v + new Vector2(_vOffset, -_vOffset),
+                v + new Vector2(-_vOffset, -_vOffset)
+            };
+            mesh.triangles = new int[] { 0, 2, 1, 2, 3, 1 };
+
+            renderer.material = new Material(_simpleMaterial);
+            renderer.material.color = Color.red;
+        }
+
+        #endregion Debug
     }
 }
