04348e33e9c95b64b3f4b341236161ac21f65ea6
Gurix525 <gurix525@gmail.com>
2022-11-05 01:33:20 +0100

    Update GravityPoint.cs

--- MicroSpace/Assets/Code/Temporary/GravityPoint.cs
+++ MicroSpace/Assets/Code/Temporary/GravityPoint.cs
@@ -1,18 +1,28 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GravityPoint : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private static readonly float _gravityConstant = 400000F;
+
+    private void FixedUpdate()
     {
-        
+        foreach (Transform child in transform.parent)
+        {
+            var rigidbody = child.GetComponent<Rigidbody2D>();
+            if (child != transform && rigidbody != null)
+            {
+                rigidbody.AddForce(CalculateForce(child));
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private Vector2 CalculateForce(Transform child)
     {
-        
+        var distance = Vector2.Distance(child.position, transform.position);
+        return (transform.position - child.position).normalized *
+            _gravityConstant / (distance * distance);
     }
 }
