ae099b463f3e9a99f029be6d113983813c8f97ff
Gurix525 <gurix525@gmail.com>
2022-12-06 21:13:00 +0100

    Added task.cs

---
+++ MicroSpace/Assets/Code/Tasks/Task.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Miscellaneous;
+
+namespace Tasks
+{
+    public class Task : IIdentifiable
+    {
+        public int Id { get; }
+        public Dictionary<int, int> Items { get; } = new();
+        public int ToolModel { get; }
+        public float ToolPower { get; }
+        public int SkillModel { get; }
+        public float SkillPower { get; }
+    }
+}
