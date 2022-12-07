bdffb7c6d40222369b41062e4f6d9a30cd573306
Gurix525 <gurix525@gmail.com>
2022-12-07 02:25:29 +0100

    Update MassItem.cs

--- MicroSpace/Assets/Code/Entities/RigidEntity/MassItem.cs
+++ MicroSpace/Assets/Code/Entities/RigidEntity/MassItem.cs
@@ -1,6 +1,14 @@
+using Attributes;
+using UnityEngine;
+
 namespace Entities
 {
     public class MassItem : RigidEntity
     {
+        [field: SerializeField, ReadonlyInspector]
+        public int ItemModel { get; private set; }
+
+        [field: SerializeField, ReadonlyInspector]
+        public float Mass { get; private set; }
     }
 }
