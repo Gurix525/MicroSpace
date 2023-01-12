eb120bc8bb4987bcb7a46ba88be0c8d4105f51d1
Gurix525 <gurix525@gmail.com>
2023-01-12 03:28:23 +0100

    Sun nearly done

--- MicroSpace/Assets/Code/Entities/Block/SolidBlock/Wall.cs
+++ MicroSpace/Assets/Code/Entities/Block/SolidBlock/Wall.cs
@@ -1,7 +1,23 @@
-﻿namespace Entities
+﻿using UnityEngine;
+
+namespace Entities
 {
     public class Wall : SolidBlock
     {
+        private SpriteMask _spriteMask;
+        private SpriteRenderer _renderer;
         public bool IsIncludedInObstacle { get; set; }
+
+        protected override void Awake()
+        {
+            base.Awake();
+            _spriteMask = GetComponent<SpriteMask>();
+            _renderer = GetComponent<SpriteRenderer>();
+        }
+
+        private void FixedUpdate()
+        {
+            _spriteMask.sortingLayerName = _renderer.sortingLayerName;
+        }
     }
 }
